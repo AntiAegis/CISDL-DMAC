4a4e24051dedb4e534291a71ec32571b07ba7217
AntiAegis <thuypn9a4@gmail.com>
2019-06-02 21:38:16 +0700

    Update demo

--- demo.py
+++ demo.py
@@ -29,12 +29,12 @@ class SoftmaxMask(nn.Module):
 #------------------------------------------------------------------------------
 #   vis_fun
 #------------------------------------------------------------------------------
-def vis_fun(output0,output1,outputname1,outputname2):
+def vis_fun(output0, output1, outputname1, outputname2):
 	softmax_mask = SoftmaxMask()
 	
 	output0 = softmax_mask(output0).cpu().data[0].numpy()
 	output1 = softmax_mask(output1).cpu().data[0].numpy()
-		
+
 	output0 = np.uint8(output0 * 255)
 	output1 = np.uint8(output1 * 255)
 	mask0 = cv2.applyColorMap(output0,cv2.COLORMAP_JET)
@@ -48,9 +48,9 @@ def vis_fun(output0,output1,outputname1,outputname2):
 #------------------------------------------------------------------------------
 def CISDL(probe, donor):
 	if os.path.exists(probe) and os.path.exists(donor):
-		print probe + ' and ' + donor + ' are proccessed!'
+		print(probe + ' and ' + donor + ' are proccessed!')
 	else:
-		print 'File none exsit!'
+		print('File none exsit!')
 		return
 	input_scale = 256
 	upsample_layer = nn.UpsamplingBilinear2d(size=(input_scale, input_scale))
@@ -60,7 +60,7 @@ def CISDL(probe, donor):
 		
 	img = cv2.imread(probe)
 	if img is None:
-		print 'Reading error occurred!'
+		print('Reading error occurred!')
 		return
 	img = img.astype(float)
 	img = cv2.resize(img,(input_scale,input_scale)).astype(float)
@@ -71,7 +71,7 @@ def CISDL(probe, donor):
 		
 	img = cv2.imread(donor)
 	if img is None:
-		print 'Reading error occurred!'
+		print('Reading error occurred!')
 		return
 	img = img.astype(float)
 	img = cv2.resize(img,(input_scale,input_scale)).astype(float)
@@ -79,13 +79,13 @@ def CISDL(probe, donor):
 	img[:,:,1] = img[:,:,1] - 116.669
 	img[:,:,2] = img[:,:,2] - 122.675
 	img2[:img.shape[0],:img.shape[1],:] = img
-	
+
 	gpu0 = 0
 	model = dmac_vgg.DMAC_VGG(2, gpu0, input_scale)
 	model.eval()
 	model.cuda(gpu0)
-	model_path = 'DMAC-adv.pth'
-	saved_state_dict = torch.load(model_path)
+	model_path = '/media/antiaegis/storing/FORGERY/segmentation/checkpoints/ForgSeg/DMAC-adv.pth'
+	saved_state_dict = torch.load(model_path, map_location='cpu')
 	model.load_state_dict(saved_state_dict)
 	
 	image1 = Variable(torch.from_numpy(img1[np.newaxis, :].transpose(0,3,1,2)).float(),volatile = True).cuda(gpu0)
@@ -93,10 +93,8 @@ def CISDL(probe, donor):
 	output = model(image1, image2)
 	output0 = upsample_layer(output[0])
 	output1 = upsample_layer(output[1])
-	outputname1 = probe[:-4]
-	outputname2 = donor[:-4]
-	vis_fun(output0,output1,outputname1,outputname2)
-	
+	return output0, output1
+
 
 #------------------------------------------------------------------------------
 #   Main execution
@@ -104,4 +102,8 @@ def CISDL(probe, donor):
 if __name__ == "__main__":
 	probe = 'im1_1.jpg'
 	donor = 'im1_2.jpg'
-	CISDL(probe, donor)      
+	outputname1 = probe[:-4]
+	outputname2 = donor[:-4]
+
+	output0, output1 = CISDL(probe, donor) 
+	vis_fun(output0, output1, outputname1, outputname2)     
